0ef42ab803c2f07e59b59b7671fcf3f81ead776c
Vinicius <vinicius.diniz444@gmail.com>
2022-01-15 19:13:28 -0300

    primeiro commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState, useEffect } from 'react';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+  const [repositories, setRepositories] = useState([]);
 
+
+  //como não é passado um parametro para verificar se uma variavel foi alterada só executa 1 vez
+  useEffect(() => {
+    async function getRepositories(){
+      const response = await fetch('https://api.github.com/users/viniciusdiniz21/repos');
+      const data = await response.json();
+      setRepositories(data);
+    }
+    getRepositories();
+  }, []);
+
+  useEffect(()=>{
+    const filtered = repositories.filter((repo) => repo.favorite)
+    document.title = `Você tem ${filtered.length} favoritos`
+  }, [repositories])
+
+
+
+
+  function handleFavorite(id){
+    const newRepo = repositories.map((repo)=>{
+      return repo.id === id ? {...repo, favorite: !repo.favorite} : repo
+    })
+    setRepositories(newRepo)
+  }
+
+  return (
+    <ul>
+      {repositories.map((repo)=>(
+        <li key = {repo.id}>
+        {repo.name}
+        {repo.favorite && <span>(Favorito)</span>}
+        <button onClick={() => handleFavorite(repo.id)}>Favoritar</button>
+        </li>
+      ))}
+    </ul>
+  );
+
+}
 export default App;
